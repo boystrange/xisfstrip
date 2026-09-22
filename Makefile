74a8b18722f70ee9d796f305c6fbae386464a259
Luca Padovani <luca.padovani@unicam.it>
2026-09-22 12:35:22 +0200

    snapshot

---
+++ Makefile
@@ -0,0 +1,7 @@
+
+all:
+
+setup:
+	python3 -m venv venv
+	bash -c "source venv/bin/activate"
+	python3 -m pip install xisf
--- src/xisfstrip.py
+++ src/xisfstrip.py
@@ -1,5 +1,6 @@
 import sys
 from xisf import XISF
+from pathlib import Path
 
 def strip(file_name):
     xisf = XISF(file_name)
@@ -7,19 +8,27 @@ def strip(file_name):
     ims_meta = xisf.get_images_metadata()
     im_data = xisf.read_image(0)
 
-    XISF.write(
-        file_name, im_data,
-        creator_app = "XISF Strip",
-        image_metadata = ims_meta[0],
-        xisf_metadata = file_meta,
-        codec='lz4hc', shuffle=True
-    )
+    if len(ims_meta) > 1 or 'XISF:CompressionCodecs' not in file_meta:
+        XISF.write(
+            file_name, im_data,
+            creator_app = "XISF Strip",
+            image_metadata = ims_meta[0],
+            xisf_metadata = file_meta,
+            codec = 'lz4hc', shuffle = True
+        )
+        return True
+    else:
+        return False
 
 if len(sys.argv) < 2:
     print('XISF Strip v1.0 by Luca Padovani (2026)')
-    print('Usage: xisfstrip file ...')
+    print('Usage: xisfstrip path')
     sys.exit(-1)
 
-for f in sys.argv[1:]:
-    print('Processing', f, '...')
-    strip(f)
+for p in Path(sys.argv[1]).rglob('*.xisf'):
+    print('Processing', p, '... ', end = '')
+    res = strip(p)
+    if res:
+        print('done')
+    else:
+        print('nothing to do')
